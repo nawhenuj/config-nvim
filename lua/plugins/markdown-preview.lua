b7facc7bd498949ba807ff7828a3153c45ef16e9
nawhenuj <cplanawhenuj@gmail.com>
2024-04-11 09:03:16 +0900

    Set refresh slow to true

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -6,6 +6,7 @@ return {
     config = function()
         vim.g.mkdp_auto_start = 1
         vim.g.mkdp_auto_close = 0
+        vim.g.mkdp_refresh_slow = 1
         vim.g.mkdp_combine_preview = 1
         vim.g.mkdp_combine_preview_auto_refresh = 1
     end,
